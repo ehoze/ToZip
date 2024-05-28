468aff4ba0c765277e9ab653a1c857c8979061fb
ehoze <eryk_kucharski@tlen.pl>
2024-05-28 20:58:57 +0200

    # Code refactor and denesting - changed language of messages to english.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// ehz
+using System;
 using System.IO;
 using System.IO.Compression;
 
@@ -8,36 +9,33 @@ namespace ToZip
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0) 
-            {
-                Console.WriteLine("Przeciągnij tutaj folder aby go spakować do pliku .zip");
-            }
-
             string folderPath = args[0];
-            
-            if (Directory.Exists(folderPath))
+
+            if (string.IsNullOrWhiteSpace(folderPath))
             {
-                string zipFilePath = folderPath.TrimEnd(Path.DirectorySeparatorChar) + ".zip";
-    
-                try
-                {
-                    if(File.Exists(zipFilePath))
-                    {
-                        File.Delete(zipFilePath);
-                    }
-
-                    ZipFile.CreateFromDirectory(folderPath, zipFilePath);
-                    Console.WriteLine($"Folder {folderPath} został spakowany do pliku {zipFilePath}");
-
-                }
-                catch (Exception ex) 
-                {
-                    Console.WriteLine($"Wystąpił błąd: {ex.Message}");
-                }
+                Console.WriteLine("Podana ścieżka jest pusta lub nieprawidłowa.");
+                return;
             }
-            else
+            if (!Directory.Exists(folderPath))
             {
                 Console.WriteLine("Podana ścieżka nie jest prawidłowym folderem.");
+                return;
+            }
+
+            string zipFilePath = folderPath.TrimEnd(Path.DirectorySeparatorChar) + ".zip";
+            try
+            {
+                if (File.Exists(zipFilePath))
+                {
+                    File.Delete(zipFilePath);
+                }
+                ZipFile.CreateFromDirectory(folderPath, zipFilePath);
+                Console.WriteLine($"Folder {folderPath} został spakowany do pliku {zipFilePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Wystąpił błąd: {ex.Message}");
+                return;
             }
         }
     }
